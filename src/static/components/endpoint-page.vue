322b54999c4c5e818ec2c0025d90a1b353d50aba
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 00:30:33 +0800

    Render app-header component inside endpoint-page

--- src/static/components/endpoint-page.vue
+++ src/static/components/endpoint-page.vue
@@ -1,5 +1,13 @@
 <template>
     <div class="endpoint-page">
-        <h1>Endpoint Page</h1>
+        <app-header></app-header>
     </div>
 </template>
+
+<script>
+    export default {
+        components: {
+            'app-header': require('./app-header.vue').default
+        }
+    };
+</script>
