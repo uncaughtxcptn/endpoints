c62988770625fe598d679d648b25b0334624640b
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 00:08:24 +0800

    Add create endpoint button in landing page

--- src/static/components/landing-page.vue
+++ src/static/components/landing-page.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>Landing Page</h1>
+    <div class="landing-page">
+        <button @click="onClick">Create Endpoint</button>
+    </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            onClick(e) {
+                // TODO: Get actual endpoint id/hash from backend.
+                const hash = 'abcdefghijklmnopqrstuvwxyz';
+
+                this.$router.push({
+                    name: 'endpoint-page',
+                    params: { hash }
+                });
+            }
+        }
+    };
+</script>
