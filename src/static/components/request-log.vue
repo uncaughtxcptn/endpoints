43146dd4b02415560ab2ba9143011f6aed18d833
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 01:21:53 +0800

    Initial work on request-log html markup

--- src/static/components/request-log.vue
+++ src/static/components/request-log.vue
@@ -1,5 +1,25 @@
 <template>
     <section class="request-log">
+        <header>
+            <p class="method">GET</p>
+            <time class="timestamp" :datetime="data.timestamp">{{ date }}</time>
+        </header>
+
         <p>Request Log</p>
     </section>
 </template>
+
+<script>
+    import format from 'date-fns/format';
+
+    export default {
+        props: ['data'],
+
+        computed: {
+            date() {
+                const date = new Date(this.data.timestamp);
+                return format(date, 'MMM D, YYYY hh:mm:ss a');
+            }
+        }
+    };
+</script>
