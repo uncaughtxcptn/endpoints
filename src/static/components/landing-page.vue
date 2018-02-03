565220633527c7e041babce10fdc8cab99d91907
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 00:24:42 +0800

    Use app-header component inside landing-page

--- src/static/components/landing-page.vue
+++ src/static/components/landing-page.vue
@@ -1,21 +1,13 @@
 <template>
     <div class="landing-page">
-        <button @click="onClick">Create Endpoint</button>
+        <app-header></app-header>
     </div>
 </template>
 
 <script>
     export default {
-        methods: {
-            onClick(e) {
-                // TODO: Get actual endpoint id/hash from backend.
-                const hash = 'abcdefghijklmnopqrstuvwxyz';
-
-                this.$router.push({
-                    name: 'endpoint-page',
-                    params: { hash }
-                });
-            }
+        components: {
+            'app-header': require('./app-header.vue').default
         }
     };
 </script>
