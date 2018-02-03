709d8d117077c80e8811c94f7ee7c8b3f010c991
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 00:38:07 +0800

    Add beforeRouteEnter/Leave guards to endpoint-page component

--- src/static/components/endpoint-page.vue
+++ src/static/components/endpoint-page.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+    import store from '../store';
+
     export default {
         components: {
             'app-header': require('./app-header.vue').default
+        },
+
+        beforeRouteEnter(to, from, next) {
+            store.commit('setHash', to.params.hash);
+            next();
+        },
+
+        beforeRouteLeave(to, from, next) {
+            store.commit('unsetHash');
+            next();
         }
     };
 </script>
