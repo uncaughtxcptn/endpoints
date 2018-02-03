8973395b63330902222049ccf1295c643d3d221e
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 00:37:13 +0800

    Add store state and mutations for the endpoint hash

--- src/static/store.js
+++ src/static/store.js
@@ -4,5 +4,17 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {}
+    state: {
+        hash: null
+    },
+
+    mutations: {
+        setHash(state, hash) {
+            state.hash = hash;
+        },
+
+        unsetHash(state) {
+            state.hash = null;
+        }
+    }
 });
