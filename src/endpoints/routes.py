762be2a42cb0805ab91f92e8282b42e26bc7ca3e
Emmanuel Lodovice <name3anad@gmail.com>
2018-02-04 17:14:03 +0800

    Added view for creating new endpoints

--- src/endpoints/routes.py
+++ src/endpoints/routes.py
@@ -1,10 +1,11 @@
-from views import index
+from views import index, create_endpoint
 
 from pathlib import Path
 
 
 def setup_routes(app):
     app.router.add_get('/', index)
+    app.router.add_get('/endpoint', create_endpoint)
     app.router.add_get('/{hash}', index)
     app.router.add_static(
         '/static/', path=str(Path('.') / 'static'), name='static')
--- src/endpoints/views.py
+++ src/endpoints/views.py
@@ -1,6 +1,22 @@
 from aiohttp_jinja2 import template
+from aiohttp import web
+
+from db import Endpoint
+
+import uuid
 
 
 @template('index.html')
 async def index(request):
     pass
+
+
+async def create_endpoint(request):
+    db = request.app['db']
+    async with db.acquire() as conn:
+        async with conn.begin():
+            hash_value = str(uuid.uuid4())
+            data = await conn.execute(
+                Endpoint.__table__.insert().values({'hash': hash_value}))
+            data = await data.fetchone()
+    return web.json_response({'id': data.id, 'hash': hash_value})
