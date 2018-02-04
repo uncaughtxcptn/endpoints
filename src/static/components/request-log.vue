fcba5046653816e7429e9961fa019592f4b76d7f
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 12:14:13 +0800

    Add example response details in request-log component

--- src/static/components/request-log.vue
+++ src/static/components/request-log.vue
@@ -1,14 +1,24 @@
 <template>
     <section class="request-log">
-        <header>
+        <header class="section">
             <p class="method">GET</p>
             <time class="timestamp" :datetime="data.timestamp">{{ date }}</time>
         </header>
 
-        <div class="request-details">
+        <div class="section request-details">
             <p>GET /abcdefghijklmnopqrstuvwxyz HTTP/1.1</p>
             <p>Host: localhost:3000</p>
             <p>Accept: application/json</p>
+        </div>
+
+        <div class="section response-details">
+            <p>HTTP/1.1 200 OK</p>
+            <p>Content-Type: application-json</p>
+            <p>Content-Length: 100</p>
+            <p>Cache-Control: public, max-age=10000</p>
+            <p>Etag: w/kdi283lsldjlsjds</p>
+            <br>
+            <p>{"title":"Hello World"}</p>
         </div>
     </section>
 </template>
@@ -36,14 +46,18 @@
         background-color: #fff;
     }
 
+    .section:not(:last-child) {
+        border-bottom: 1px solid var(--gray-1);
+    }
+
     header {
         display: flex;
         justify-content: space-between;
         padding: 0.4rem 0.8rem;
-        border-bottom: 1px solid var(--gray-1);
     }
 
-    .request-details {
+    .request-details,
+    .response-details {
         padding: 0.4rem 0.8rem;
     }
 </style>
