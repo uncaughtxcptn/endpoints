b36c1ff67acebdb0e5f08d6ab30d686819c153ac
Emmanuel Lodovice <name3anad@gmail.com>
2018-02-04 21:08:19 +0800

    Added access logging when endpoint is visited
    Changelog:
    * added a new table for storing custom response
    * added a new view to handling the endpoint
    * added util methods for generating http message for response
    and request object

--- src/endpoints/routes.py
+++ src/endpoints/routes.py
@@ -1,11 +1,11 @@
-from views import index, create_endpoint
+from views import index, create_endpoint, visit_endpoint
 
 from pathlib import Path
 
 
 def setup_routes(app):
-    app.router.add_get('/', index)
+    app.router.add_route('GET', '/', index, name='home')
     app.router.add_get('/endpoint', create_endpoint)
-    app.router.add_get('/{hash}', index)
+    app.router.add_get('/{hash}', visit_endpoint)
     app.router.add_static(
         '/static/', path=str(Path('.') / 'static'), name='static')
