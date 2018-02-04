c55009cd83c206d6fbe9eee76839d808d5fdc0f1
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 12:06:45 +0800

    Add example request details to request-log component

--- src/static/components/request-log.vue
+++ src/static/components/request-log.vue
@@ -5,7 +5,11 @@
             <time class="timestamp" :datetime="data.timestamp">{{ date }}</time>
         </header>
 
-        <p>Request Log</p>
+        <div class="request-details">
+            <p>GET /abcdefghijklmnopqrstuvwxyz HTTP/1.1</p>
+            <p>Host: localhost:3000</p>
+            <p>Accept: application/json</p>
+        </div>
     </section>
 </template>
 
@@ -18,8 +22,28 @@
         computed: {
             date() {
                 const date = new Date(this.data.timestamp);
-                return format(date, 'MMM D, YYYY hh:mm:ss a');
+                return format(date, 'MMM D, YYYY hh:mm:ss A');
             }
         }
     };
 </script>
+
+<style scoped>
+    .request-log {
+        border: 1px solid var(--gray-1);
+        border-radius: 2px;
+        font-size: 1.2rem;
+        background-color: #fff;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        padding: 0.4rem 0.8rem;
+        border-bottom: 1px solid var(--gray-1);
+    }
+
+    .request-details {
+        padding: 0.4rem 0.8rem;
+    }
+</style>
