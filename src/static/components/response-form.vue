9074d55474e69e1c1b95e345fb4c273d209512a5
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 14:50:34 +0800

    Add dummy response form vue component

---
+++ src/static/components/response-form.vue
@@ -0,0 +1,11 @@
+<template>
+    <form action="#" class="response-form">
+        <p>Response Form</p>
+    </form>
+</template>
+
+<style scoped>
+    .response-form {
+        padding: 0.4rem 0.8rem;
+    }
+</style>
