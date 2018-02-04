23b136a7ff724c7342baf71b6ece907e82dad158
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 12:27:09 +0800

    Toggleable request and response details sections

--- src/static/components/request-log.vue
+++ src/static/components/request-log.vue
@@ -1,17 +1,17 @@
 <template>
     <section class="request-log">
-        <header class="section">
+        <header class="section" @click="toggleExpanded">
             <p class="method">GET</p>
             <time class="timestamp" :datetime="data.timestamp">{{ date }}</time>
         </header>
 
-        <div class="section request-details">
+        <div v-if="isExpanded" class="section request-details">
             <p>GET /abcdefghijklmnopqrstuvwxyz HTTP/1.1</p>
             <p>Host: localhost:3000</p>
             <p>Accept: application/json</p>
         </div>
 
-        <div class="section response-details">
+        <div v-if="isExpanded" class="section response-details">
             <p>HTTP/1.1 200 OK</p>
             <p>Content-Type: application-json</p>
             <p>Content-Length: 100</p>
@@ -29,10 +29,22 @@
     export default {
         props: ['data'],
 
+        data() {
+            return {
+                isExpanded: false
+            };
+        },
+
         computed: {
             date() {
                 const date = new Date(this.data.timestamp);
                 return format(date, 'MMM D, YYYY hh:mm:ss A');
+            }
+        },
+
+        methods: {
+            toggleExpanded() {
+                this.isExpanded = !this.isExpanded;
             }
         }
     };
@@ -54,6 +66,7 @@
         display: flex;
         justify-content: space-between;
         padding: 0.4rem 0.8rem;
+        cursor: pointer;
     }
 
     .request-details,
