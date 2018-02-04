f63ce55a6c48be339170b0c268bcceafe6f2359f
Arnelle Balane <arnellebalane@gmail.com>
2018-02-04 20:38:19 +0800

    Add labelled-switch vue component

---
+++ src/static/components/labelled-switch.vue
@@ -0,0 +1,32 @@
+<template>
+    <label class="labelled-switch">
+        <base-switch :checked="checked"></base-switch>
+        <span>{{ label }}</span>
+    </label>
+</template>
+
+<script>
+    export default {
+        props: ['checked', 'label'],
+
+        components: {
+            'base-switch': require('./base-switch.vue').default
+        }
+    };
+</script>
+
+<style scoped>
+    .labelled-switch {
+        display: inline-flex;
+        align-items: center;
+        cursor: pointer;
+    }
+
+    .base-switch {
+        margin-right: 0.4rem;
+    }
+
+    span {
+        font-size: 0.8em;
+    }
+</style>
