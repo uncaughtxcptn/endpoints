4699244c77881f214b741765a8bb51a2d712e669
Arnelle Balane <arnellebalane@gmail.com>
2018-02-05 20:52:00 +0800

    Update webpack config to use file-loader for png files

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,12 @@ module.exports = {
         }, {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.png$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
         } ]
     }
 };
