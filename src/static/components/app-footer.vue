c0f3947b85c63ad77308188252575b7d78fbdf1d
Arnelle Balane <arnellebalane@gmail.com>
2018-02-10 23:32:59 +0800

    Add app-footer vue component

---
+++ src/static/components/app-footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer class="app-footer">
+        <div class="wrapper">
+            <small>
+                Developed by
+                <a href="https://github.com/uncaughtxcptn" target="_blank" rel="noopener noreferrer">Uncaught Exception</a>
+            </small>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+    .app-footer {
+        padding: 1.2rem 0;
+        border-top: 1px solid var(--gray-1);
+        color: var(--gray-2);
+    }
+
+    .wrapper {
+        text-align: right;
+    }
+
+    a {
+        text-decoration: none;
+        color: var(--gray-3);
+    }
+
+    a:hover,
+    a:focus {
+        text-decoration: underline;
+    }
+</style>
--- src/static/components/landing-page.vue
+++ src/static/components/landing-page.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="landing-page">
         <app-header></app-header>
-
         <hero-section></hero-section>
         <features-section></features-section>
+        <app-footer></app-footer>
     </div>
 </template>
 
@@ -11,6 +11,7 @@
     export default {
         components: {
             'app-header': require('./app-header.vue').default,
+            'app-footer': require('./app-footer.vue').default,
             'hero-section': require('./hero-section.vue').default,
             'features-section': require('./features-section.vue').default
         }
