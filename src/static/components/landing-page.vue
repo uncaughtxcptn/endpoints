dda763cf1dd837da9de97b86159915f839c58419
Arnelle Balane <arnellebalane@gmail.com>
2018-02-10 22:33:59 +0800

    Add hero-section component to landing page

--- src/static/components/landing-page.vue
+++ src/static/components/landing-page.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="landing-page">
         <app-header></app-header>
+
+        <hero-section></hero-section>
     </div>
 </template>
 
 <script>
     export default {
         components: {
-            'app-header': require('./app-header.vue').default
+            'app-header': require('./app-header.vue').default,
+            'hero-section': require('./hero-section.vue').default
         }
     };
 </script>
