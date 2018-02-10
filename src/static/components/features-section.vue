5a5d3a44e69f9db0ec228190b9ff42654552553e
Arnelle Balane <arnellebalane@gmail.com>
2018-02-10 23:08:51 +0800

    Add features-section component in landing page

---
+++ src/static/components/features-section.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="features-section">
+        <div class="wrapper">
+            <h2>Features</h2>
+
+            <div class="features">
+                <div class="feature">
+                    <div class="img-placeholder"></div>
+                    <h4>Realtime Request Logs</h4>
+                    <p>See HTTP requests sent to your endpoint in realtime.</p>
+                </div>
+
+                <div class="feature">
+                    <div class="img-placeholder"></div>
+                    <h4>Customizeable Responses</h4>
+                    <p>Respond with custom status codes, response headers, and body.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .features-section {
+        padding: 4rem 0;
+    }
+
+    h2 {
+        margin-bottom: 4rem;
+        font-size: 1.8rem;
+        font-weight: 300;
+        text-transform: uppercase;
+        text-align: center;
+        letter-spacing: 2px;
+        color: var(--gray-3);
+    }
+
+    .features {
+        display: flex;
+    }
+
+    .feature {
+        width: 50%;
+        padding: 0 1rem;
+        text-align: center;
+    }
+
+    .img-placeholder {
+        background-color: var(--gray-1);
+    }
+
+    .img-placeholder::before {
+        content: "";
+        display: block;
+        padding-top: calc(100% / 16 * 5);
+    }
+
+    h4 {
+        margin: 1.2rem 0;
+        font-size: 1.6rem;
+        font-weight: 500;
+    }
+
+    p {
+        font-weight: 300;
+        color: var(--gray-3);
+    }
+</style>
--- src/static/components/landing-page.vue
+++ src/static/components/landing-page.vue
@@ -3,6 +3,7 @@
         <app-header></app-header>
 
         <hero-section></hero-section>
+        <features-section></features-section>
     </div>
 </template>
 
@@ -10,7 +11,8 @@
     export default {
         components: {
             'app-header': require('./app-header.vue').default,
-            'hero-section': require('./hero-section.vue').default
+            'hero-section': require('./hero-section.vue').default,
+            'features-section': require('./features-section.vue').default
         }
     };
 </script>
