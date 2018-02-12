5e5f9f81fff372e8795f95cdce98ef04fc4bbd77
Arnelle Balane <arnellebalane@gmail.com>
2018-02-12 23:57:31 +0800

    Store fetch actual endpoints list from idb

--- src/static/store.js
+++ src/static/store.js
@@ -99,11 +99,8 @@ export default new Vuex.Store({
         },
 
         async fetchAvailableEndpoints(context) {
-            context.commit('setAvailableEndpoints', [
-                { hash: '34eaa84b-c1c3-49c1-ad4c-6f12e5412f24', timestamp: Date.now() },
-                { hash: '34eaa84b-c1c3-49c1-ad4c-6f12e5412f24', timestamp: Date.now() },
-                { hash: '34eaa84b-c1c3-49c1-ad4c-6f12e5412f24', timestamp: Date.now() }
-            ]);
+            const endpoints = await endpointsDB.getAll();
+            context.commit('setAvailableEndpoints', endpoints);
         },
 
         async createEndpoint(context) {
