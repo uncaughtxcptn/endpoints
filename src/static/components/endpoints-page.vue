315d0998f511b877d094c393e9cf9b686035d8ce
Arnelle Balane <arnellebalane@gmail.com>
2018-02-12 21:12:20 +0800

    Add vue component for endpoints page

---
+++ src/static/components/endpoints-page.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="endpoints-page">
+        <app-header></app-header>
+
+        <div class="wrapper">
+            <h1>Endpoints Page</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            'app-header': require('./app-header.vue').default
+        }
+    };
+</script>
