6e24e47b44f9235936dbc4777cd65ba84ada0e57
Arnelle Balane <arnellebalane@gmail.com>
2018-02-16 01:36:59 +0800

    Initial design for the navigation-loader component

---
+++ src/static/components/navigation-loader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navigation-loader"></div>
+</template>
+
+<style scoped>
+    .navigation-loader {
+        width: 100%;
+        height: 0.2rem;
+        background: repeating-linear-gradient(
+            to right,
+            var(--primary-color) 0%,
+            var(--primary-color) 50%,
+            #fff 50%,
+            #fff 150%
+        );
+    }
+</style>
