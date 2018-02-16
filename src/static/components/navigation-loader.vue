99a20395132441cd44e83222c664963d0acf44e8
Arnelle Balane <arnellebalane@gmail.com>
2018-02-16 23:23:06 +0800

    Add navigation-loader vue component

--- src/static/components/navigation-loader.vue
+++ src/static/components/navigation-loader.vue
@@ -1,25 +1,53 @@
 <template>
-    <div class="navigation-loader">
+    <div class="navigation-loader" :class="{ visible: isLoadingNavigation }">
         <div ref="progress" class="progress"></div>
     </div>
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+
     export default {
+        data() {
+            return {
+                animation: null
+            };
+        },
+
+        computed: mapState(['isLoadingNavigation']),
+
+        watch: {
+            isLoadingNavigation(isLoadingNavigation) {
+                if (isLoadingNavigation) {
+                    this.play();
+                } else {
+                    this.finish();
+                }
+            }
+        },
+
         methods: {
-            animate() {
-                return this.$refs.progress.animate([
-                    { left: '-50%', easing: 'ease-in' },
-                    { left: '100%', easing: 'ease-out' }
-                ], 1500);
+            play() {
+                this.animation.play();
+                this.animation.addEventListener('finish', this.play, { once: true });
+            },
+
+            finish() {
+                this.animation.finish();
+                this.animation.removeEventListener('finish', this.play);
             }
         },
 
         mounted() {
-            const animateLoop = () => {
-                this.animate().onfinish = animateLoop;
-            };
-            animateLoop();
+            this.animation = this.$refs.progress.animate([
+                { left: '-50%' },
+                { left: '100%', easing: 'ease-out' }
+            ], 1500);
+            this.animation.cancel();
+
+            if (this.isLoadingNavigation) {
+                this.play();
+            }
         }
     };
 </script>
@@ -30,10 +58,14 @@
         height: 0.2rem;
     }
 
+    .navigation-loader:not(.visible) {
+        display: none;
+    }
+
     .progress {
         position: absolute;
         top: 0;
-        left: 0;
+        left: -50%;
         width: 50%;
         height: 100%;
         background-color: var(--primary-color);
