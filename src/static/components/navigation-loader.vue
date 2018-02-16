530382a41b933e76272b8cf9b0a0b8e5207c5583
Arnelle Balane <arnellebalane@gmail.com>
2018-02-16 22:41:42 +0800

    Run navigation-loader animation loop

--- src/static/components/navigation-loader.vue
+++ src/static/components/navigation-loader.vue
@@ -1,8 +1,28 @@
 <template>
     <div class="navigation-loader">
-        <div class="progress"></div>
+        <div ref="progress" class="progress"></div>
     </div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            animate() {
+                return this.$refs.progress.animate([
+                    { left: '-50%', easing: 'ease-in' },
+                    { left: '100%', easing: 'ease-out' }
+                ], 1500);
+            }
+        },
+
+        mounted() {
+            const animateLoop = () => {
+                this.animate().onfinish = animateLoop;
+            };
+            animateLoop();
+        }
+    };
+</script>
 
 <style scoped>
     .navigation-loader {
