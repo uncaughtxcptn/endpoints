01710a271a2bc88ff712f90335a48a2e2da30a12
Arnelle Balane <arnellebalane@gmail.com>
2018-02-24 22:47:35 +0800

    Define generic base-button vue component

---
+++ src/static/components/base-button.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="base-button" @click="onClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: ['loading'],
+
+        methods: {
+            onClick(e) {
+                this.$emit('click', e);
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    button {
+        padding: 0.5em 1em;
+        border-radius: 2px;
+        font-size: 1.3rem;
+        text-transform: uppercase;
+        cursor: pointer;
+
+        /* Default button styles */
+        border: 1px solid var(--gray-1);
+    }
+
+    .primary {
+        border: 1px solid var(--primary-color-dark-1);
+        color: #fff;
+        background-color: var(--primary-color);
+    }
+</style>
