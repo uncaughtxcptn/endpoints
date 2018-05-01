1ba81831b4e3623e317e22220149721ffcf64487
Arnelle Balane <arnellebalane@gmail.com>
2018-05-01 22:58:18 +0800

    Replace style-loader with vue-style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             loader: 'vue-loader'
         }, {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: ['vue-style-loader', 'css-loader']
         }, {
             test: /\.png$/,
             loader: 'url-loader',
