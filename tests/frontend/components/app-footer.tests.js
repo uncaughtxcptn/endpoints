3603d98f340a39056b8489236fcf5dadc2f202d6
Arnelle Balane <arnellebalane@gmail.com>
2018-10-21 01:16:41 +0800

    Define shallowMount-ed app-footer for each test case

--- tests/frontend/components/app-footer.tests.js
+++ tests/frontend/components/app-footer.tests.js
@@ -2,17 +2,18 @@ import { shallowMount } from '@vue/test-utils';
 import AppFooter from '@/components/app-footer.vue';
 
 describe('AppFooter', () => {
-    const wrapper = shallowMount(AppFooter);
-
     test('is a Vue instance', () => {
+        const wrapper = shallowMount(AppFooter);
         expect(wrapper.isVueInstance()).toBeTruthy();
     });
 
     test('matches snapshot', () => {
+        const wrapper = shallowMount(AppFooter);
         expect(wrapper.element).toMatchSnapshot();
     });
 
     test('has a link to UncaughtException GitHub profile', () => {
+        const wrapper = shallowMount(AppFooter);
         const link = wrapper.find('small a');
         expect(link.attributes('href')).toBe('https://github.com/uncaughtxcptn');
         expect(link.attributes('target')).toBe('_blank');
@@ -20,6 +21,7 @@ describe('AppFooter', () => {
     });
 
     test('has a link to project\'s GitHub repository', () => {
+        const wrapper = shallowMount(AppFooter);
         const link = wrapper.find('.github-link');
         expect(link.attributes('href')).toBe('https://github.com/uncaughtxcptn/endpoints');
         expect(link.attributes('target')).toBe('_blank');
